e150c89e1536e08a23db6f4cfb3a165190d4e6d2
DenysOlkhovykDev <denysolkhovykdev@gmail.com>
2026-08-18 10:55:54 +0300

    109-make-a-debug-system

--- src/resources/resource.ts
+++ src/resources/resource.ts
@@ -2,21 +2,79 @@ import { Graphics, Texture, Container } from "pixi.js";
 
 export abstract class Resource {
   root: Container;
-  isReserved: boolean;
-  isReservedForTransport: boolean;
-  isReservedForConstruction: boolean;
+
+  reservationMark: Graphics = new Graphics();
+
+  private _isReserved = false;
+  private _isReservedForTransport = false;
+  private _isReservedForConstruction = false;
 
   static baseTexture: Texture;
 
   constructor(public resourceType: string) {
     this.root = new Container();
-    this.draw();
     this.root.x = 0;
     this.root.y = 0;
     this.isReserved = false;
     this.isReservedForTransport = false;
     this.isReservedForConstruction = false;
+
+    this.draw();
+
+    this.root.addChild(this.reservationMark);
+  }
+
+  get isReserved(): boolean {
+    return this._isReserved;
+  }
+
+  set isReserved(value: boolean) {
+    this._isReserved = value;
+    this.updateReservation();
+  }
+
+  get isReservedForTransport(): boolean {
+    return this._isReservedForTransport;
+  }
+
+  set isReservedForTransport(value: boolean) {
+    this._isReservedForTransport = value;
+    this.updateReservation();
+  }
+
+  get isReservedForConstruction(): boolean {
+    return this._isReservedForConstruction;
+  }
+
+  set isReservedForConstruction(value: boolean) {
+    this._isReservedForConstruction = value;
+    this.updateReservation();
   }
 
   protected abstract draw(): void;
+
+  protected updateReservation() {
+    if (import.meta.env.VITE_IS_DEBUG !== "true") {
+      return;
+    }
+
+    this.reservationMark.clear();
+
+    if (this.isReserved) {
+      this.reservationMark.circle(0, 0, 10).fill("#ff0000");
+    }
+    if (this.isReservedForTransport) {
+      this.reservationMark.circle(0, 0, 8).fill("#00ff00");
+    }
+    if (this.isReservedForConstruction) {
+      this.reservationMark.circle(0, 0, 6).fill("#0000ff");
+    }
+
+    // console.log(
+    //   this.constructor.name,
+    //   this.isReserved,
+    //   this.isReservedForTransport,
+    //   this.isReservedForConstruction,
+    // );
+  }
 }
